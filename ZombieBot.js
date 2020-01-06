1eeaec421f0c48598eff9c59e2762bf022dbd7b4
GiuGames9012 <59582255+GiuGames9012@users.noreply.github.com>
2020-01-06 16:10:10 -0500

    Add files via upload

---
+++ ZombieBot.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+const  client = new Discord.Client();
+const config = require("./config.json");
+
+client.on("ready", () => {
+   console.log("Bot iniciado correctamente!");
+   client.user.setPresence( {
+       status: "online",
+       game: {
+           name: `Saludar a todos`,
+           type: "PLAYING"
+       }
+    });
+});
+client.on("guildMemberAdd", (member) => {
+   console.log(`Nuevo usuario:  ${member.user.username} se ha unido a ${member.guild.name}.`);
+   var canal = client.channels.get('663314097999446026');
+   canal.send(`${member.user}, bienvenido a la familia de los zombies :tada: :hugging:!`);
+
+});
+
+client.on('guildMemberRemove',(member) => {
+    canal.send(`**${member.username}** se ha ido F :slight_frown:`);
+});
+
+var prefix = config.prefix;
+
+client.on("message", (message) => {
+  if (message.content.startsWith(prefix + "ping")) {
+    message.channel.send("pong!");
+  }
+  if (message.content.startsWith("hola")) {
+    message.channel.send("Hola " + message.author + " que tal?");
+  }
+});
+
+
+client.login(process.env.bot_token);
